518b3b36cdbb0a943025409f0ae029e8f7ad2dee
Chang Xiong <cnxiong03@gmail.com>
2020-01-01 20:58:21 -0500

    setTimeout function

---
+++ index.js
@@ -0,0 +1,15 @@
+function startHomework() {
+    setTimeout(() => {
+        console.log('Start homework...')
+    }, 3000);
+};
+
+startHomework();
+
+function finishHomework() {
+    setTimeout(() => {
+        console.log('Finished homework...')
+    }, 1000);
+};
+
+finishHomework();
